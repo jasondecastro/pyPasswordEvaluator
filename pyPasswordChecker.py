6c805f863810c439ff7f8478a901f23b21706891
Jason Decastro <jasonruben@live.com>
2014-02-17 20:04:11 -0500

    PEP8
    Re-factored most of the code to follow PEP8 guidelines.

--- pyPasswordChecker.py
+++ pyPasswordChecker.py
@@ -4,46 +4,53 @@
 # __license__ = GPL v2
 # This script evaluates passwords.
 
+'''
+ts = too short
+nu = needs uppercase
+nl = needs lowercase
+nn = needs number
+'''
+
 import getpass, string
 
 def testPassword():
-	password_too_short, password_needs_uppercase, password_needs_lowercase, password_needs_number = True, True, True, True
-	conditions_met = 0
-	conditions_message = ''
+    password_ts, password_nu, password_nl, password_nn = True, True, True, True
+    conditions_met = 0
+    conditions_message = ''
 
-	password = getpass.getpass('What\'s the password that you want to test?')
+    password = getpass.getpass('What\'s the password that you want to test?')
 
-	if len(password) > 6:
-		conditions_met += 1
-		password_too_short = False
+    if len(password) > 6:
+        conditions_met += 1
+        password_too_short = False
 
-	for char in password:
-		if char in string.ascii_uppercase:
-			conditions_met += 1
-			password_needs_uppercase = False
+    for char in password:
+        if char in string.ascii_uppercase:
+            conditions_met += 1
+            password_needs_uppercase = False
 
-		if char in string.ascii_lowercase:
-			conditions_met += 1
-			password_needs_lowercase = False
+    if char in string.ascii_lowercase:
+            conditions_met += 1
+            password_needs_lowercase = False
 
-		if char in string.digits:
-			conditions_met += 1
-			password_needs_number = False
+    if char in string.digits:
+            conditions_met += 1
+            password_needs_number = False
 
-	if conditions_met == 0: conditions_message = 'Your password is very weak.'
-	if conditions_met == 1: conditions_message = 'Your password is weak.'
-	if conditions_met == 2: conditions_message = 'Your password is average.'
-	if conditions_met == 3: conditions_message = 'Your password is awesome.'
+    if conditions_met == 0: conditions_message = 'Your password is very weak.'
+    if conditions_met == 1: conditions_message = 'Your password is weak.'
+    if conditions_met == 2: conditions_message = 'Your password is average.'
+    if conditions_met == 3: conditions_message = 'Your password is awesome.'
 
-	print(conditions_message)
+    print(conditions_message)
 
-	if any((password_too_short, password_needs_uppercase, password_needs_lowercase, password_needs_number)):
-		print('')
-		print('You can improve your password by...')
-		if password_needs_lowercase: print('    -using lowercase letters.')
-		if password_needs_uppercase: print('    -using uppercase letters.')
-		if password_too_short: print('    -using more characters.')
-		if password_needs_number: print('    -using numbers.')
+    if any((password_ts, password_nu, password_nl, password_nn)):
+        print('')
+        print('You can improve your password by...')
+        if password_nl: print('    -using lowercase letters.')
+        if password_nu: print('    -using uppercase letters.')
+        if password_ts: print('    -using more characters.')
+        if password_nn: print('    -using numbers.')
 
 if __name__ == '__main__':
-	testPassword()
+    testPassword()
